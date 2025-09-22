a21c1fa0f0443e723e4ecba35f92e347047bd40e
zz <zz@ZhangZhengdeMacBook-Pro.local>
2025-09-22 11:09:54 +0800

    chore: change database name from squads to rwa

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -98,7 +98,7 @@ var db *gorm.DB
 
 // ---------- DB Init ----------
 func initDB() {
-	dsn := "root:123456@tcp(127.0.0.1:3306)/squads?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(127.0.0.1:3306)/rwa?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
